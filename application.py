0924132b6a5dde830daba2ea4308ca2c5bbf7ade
Alex Cao <caoa@umich.edu>
2019-09-24 22:51:31 -0400

    Fixed bug wrt username; added validators

--- application.py
+++ application.py
@@ -3,6 +3,7 @@ from flask_login import LoginManager, login_required, login_user, logout_user, U
 from flask_bcrypt import Bcrypt
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 import os
 
 application = Flask(__name__)
@@ -33,14 +34,18 @@ class User(UserMixin):
         print('setting value')
         self._is_authenticated = value
 
-    def authenticate(self, pw_raw):
-        tf = bcrypt.check_password_hash(self.password, pw_raw)
-        print('authuser', tf)
+    def authenticate(self, username, pw_raw):
+        if self.username == username:
+            tf = bcrypt.check_password_hash(self.password, pw_raw)
+            print('pwd', tf)
+        else:
+            tf = False
+            print('username is', tf)
         self.is_authenticated = tf
 
 class LoginForm(FlaskForm):
-    username = StringField('Username')
-    password = PasswordField('Password')
+    username = StringField('Username', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 user1 = User('flask@login', os.getenv('User'), os.getenv('Pwd'), False)
@@ -50,7 +55,7 @@ def load_user(user_id):
     user1.email = user_id
     return user1
 
-@application.route('/home', methods=['GET'])
+@application.route('/', methods=['GET'])
 @login_required
 def hello():
     return render_template("hello.html")
@@ -67,7 +72,7 @@ def login():
         return redirect(url_for('beauty'))
     form = LoginForm()
     if form.validate_on_submit():
-        user1.authenticate(form.password.data)
+        user1.authenticate(form.username.data, form.password.data)
         if user1 and user1.is_authenticated:
             login_user(user1, remember=True)
             return user1 and redirect(url_for('hello'))
